8b0e023eaba5bbef1058bd84be499183b011b392
Namrata <Namrata@LAPTOP-GI4RKPFU.home>
2019-04-16 22:16:41 -0400

    new code

--- test/src/test/mytest.java
+++ test/src/test/mytest.java
@@ -5,6 +5,11 @@ public class mytest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("hello java");
+   int a=10;
+   int b=20;
+   int c= a+b;
+   System.out.println("sum:" +c);
+    
 	}
 
 }
